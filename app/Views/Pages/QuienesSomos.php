36bd884d2f5d148c568c1c541403624cbbe2f3e6
IvanRomeroMaurin <ivanromeromaurin11@gmail.com>
2025-04-25 06:14:02 -0300

    Se desarrollo la pagina Quienes Somos agregando Informacion de la empresa y el Staff

--- app/Views/Pages/QuienesSomos.php
+++ app/Views/Pages/QuienesSomos.php
@@ -1,20 +1,77 @@
-<div class="container-fluid vh-100 d-flex align-items-center">
-    <div class="row w-100">
-      <!-- Columna de texto -->
-      <div class="col-12 col-md-6 p-5 bg-light d-flex flex-column justify-content-center">
-        <h1 class="mb-4">Quiénes Somos</h1>
-        <p>
-        En L'Air Pur, nos dedicamos a ofrecer una experiencia única para los amantes de las
-        fragancias. A través de nuestra tienda online, te brindamos acceso a una amplia variedad
-        de perfumes de todas las marcas, para que encuentres el aroma que mejor se adapte a tu
-        estilo y personalidad. Desde nuestra sede en Corrientes Capital, trabajamos para hacer
-        llegar lo mejor de la perfumería mundial a tu hogar, con un servicio confiable y fácil de usar.
-        </p>
-      </div>
+<link rel="stylesheet" href="assets/css/Style-QuienesSomos.css">
 
-      <!-- Columna de imagen -->
-      <div class="col-12 col-md-6 d-flex justify-content-center align-items-center bg-white">
-        <img src="assets/img/QuienesSomosDueños.jpg" alt="Imagen representativa" class="img-fluid rounded">
+
+<div class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-2 justify-content-center m-3">
+        <div class="col mb-4 d-flex justify-content-center align-items-center">
+            <img src="assets/img/QuienesSomosLogo.png" alt="Socios" class="img-fluid rounded">
+        </div>
+        <div class="seccionTexto col mb-4 bg-light d-flex flex-column justify-content-center">
+            <h1 class="mb-2">Quiénes Somos</h1>
+            <p class="parrafo">
+            L'Air Pur es el sueño compartido de dos socios apasionados por el mundo de las fragancias.
+            Unidos por una visión común y un profundo respeto por el arte de la perfumería, fundamos
+            esta tienda con el objetivo de crear un espacio donde cada persona pueda encontrar su esencia. 
+            Combinamos experiencia, creatividad y compromiso para ofrecer una propuesta fresca, moderna y cercana.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-2 justify-content-center m-3">
+        <div class="seccionTexto col mb-4 bg-light d-flex flex-column justify-content-center">
+            <h1 class="mb-2">Nuestra Trayectoria</h1>
+            <p class="parrafo">
+            L'Air Pur nació con el deseo de acercar la magia de las fragancias a todos los rincones del mundo.
+            Fundada en Corrientes Capital, nuestra tienda comenzó con una simple idea: ofrecer perfumes
+            de calidad a través de una plataforma online que pudiera ser accesible para todos. Desde sus
+            primeros días, hemos crecido con la pasión por la perfumería y un compromiso constante con la excelencia, convirtiéndonos en un referente en el mundo de las fragancias en línea.
+            </p>
+        </div>
+        <div class="col mb-4 d-flex justify-content-center align-items-center">
+            <img src="assets/img/Trayectoria.jpg" alt="Trayectoria" class="img-fluid rounded">
+        </div>
+    </div>
+</div>
+
+
+<hr>
+    <h2 class="text-center text-uppercase mt-4 mb-4 display-6">Staff</h2>
+<hr class="mb-4">
+
+<div class="container">
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-center">
+    <div class="col mb-4">
+      <div class="card">
+        <img src="assets/img/NicolasFerraro.png" class="card-img-top" alt="Nicolás">
+        <div class="card-body">
+          <h3 class="card-title">Nicolás Ferraro</h3>
+          <h5 class="card-title">Co-fundador – Direccion Creativa</h5>
+          <p class="card-text">Nicolás es el corazón artístico de L'Air Pur. Desde muy joven se sintio atraido por el mundo de las
+            fragancias, y hoy canaliza esa pasión en la curaduría de perfumes que combinan estilo, elegancia y personalidad. Su objetivo
+            es que cada cliente encuentre un aroma que lo represente y lo acompañe en cada etapa de su vida.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col mb-4">
+      <div class="card">
+        <img src="assets/img/SebastianArmand.png" class="card-img-top" alt="Sebastián">
+        <div class="card-body">
+          <h3 class="card-title">Sebastián Armand</h3>
+          <h5 class="card-title">Co-fundador – Dirección Estratégica</h5>
+          <p class="card-text">Sebastián es el motor comercial de L'Air Pur. Su experiencia en negocios
+            digitales y su compromiso con la excelencia hacen posible que la tienda crezca cada día, ofreciendo un servicio ágil, seguro
+            y pensado para el cliente moderno. Cree en la fuerza de una buena experiencia de compra y en el poder de una fragancia bien elegida.</p>
+        </div>
       </div>
     </div>
   </div>
+</div>
+
+
+
+
+
+
--- app/Views/Pages/TerminosYCondiciones.php
+++ app/Views/Pages/TerminosYCondiciones.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="assets/css/Style-TyC.css">
+
 <section id="terminos-condiciones" class="container py-5">
   <div class="row justify-content-center">
     <div class="col-12 col-md-10 col-lg-8">
