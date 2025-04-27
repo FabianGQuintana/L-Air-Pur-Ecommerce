c2545a1188e0626005a8217d9189d9c2351bf2cf
FabianKin <fabianquintana500@gmail.com>
2025-04-27 20:18:24 -0300

    Se Incorporo al proyecto el archivo "Composer" para la seccion de "Contacto".

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codeigniter4/framework',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '42dd06e88f8ef505b9eda157aa3ea0b1ad455cfd',
+        'reference' => 'f3599c125e11b22f193329c2259ef5ae1b38b1b3',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,7 +31,7 @@
         'codeigniter4/framework' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '42dd06e88f8ef505b9eda157aa3ea0b1ad455cfd',
+            'reference' => 'f3599c125e11b22f193329c2259ef5ae1b38b1b3',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
