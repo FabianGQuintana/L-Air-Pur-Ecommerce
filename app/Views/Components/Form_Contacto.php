439fa76fda0598f63149195e5c5a6db54ac3dfcd
IvanRomeroMaurin <ivanromeromaurin11@gmail.com>
2025-04-28 02:05:56 -0300

    Modificaciones en el formulario

--- app/Views/Components/Form_Contacto.php
+++ app/Views/Components/Form_Contacto.php
@@ -1,11 +1,11 @@
-<link rel="stylesheet" href="assets/css/Style-Contacto.css">
+
 <!-- ============================
     SECCIÓN: CONTACTO
 =============================== -->
 <div class="container my-5">
     <div class="card shadow">
         <div class="card-body">
-            <h2 class="card-title mb-4 text-center">Contactanos</h2>
+             <h2 class="card-title mb-4 text-center fw-light display-6">Envianos tu consulta</h2>
 
             <?php if (session()->getFlashdata('message')): ?>
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -22,33 +22,33 @@
             <?php endif; ?>
 
             <form action="<?= base_url('Contact/send') ?>" method="post">
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nombre:</label>
-                    <input type="text" class="form-control" id="name" name="name" 
-                           value="<?= old('name') ?>" required>
-                </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">Nombre:</label>
+                <input type="text" class="form-control" id="name" name="name" 
+                    value="<?= old('name') ?>" required placeholder="Ej: Juan Pérez">
+            </div>
 
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email:</label>
-                    <input type="email" class="form-control" id="email" name="email" 
-                           value="<?= old('email') ?>" required>
-                </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" 
+                    value="<?= old('email') ?>" required placeholder="Ej: juanperez@email.com">
+            </div>
 
-                <div class="mb-3">
-                    <label for="phone" class="form-label">Teléfono:</label>
-                    <input type="text" class="form-control" id="phone" name="phone" 
-                           value="<?= old('phone') ?>" required>
-                </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Teléfono:</label>
+                <input type="text" class="form-control" id="phone" name="phone" 
+                    value="<?= old('phone') ?>" required placeholder="Ej: (3774)-504134">
+            </div>
 
-                <div class="mb-3">
-                    <label for="message" class="form-label">Mensaje:</label>
-                    <textarea class="form-control" id="message" name="message" rows="5" required><?= old('message') ?></textarea>
-                </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Mensaje:</label>
+                <textarea class="form-control" id="message" name="message" rows="5" required placeholder="Escribe tu consulta aquí..."><?= old('message') ?></textarea>
+            </div>
 
-                <div class="d-grid">
-                    <button type="submit" class="btn btn-dark btn-lg">Enviar</button>
-                </div>
-            </form>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-dark btn-lg">Enviar</button>
+            </div>
+        </form>
         </div>
     </div>
 </div>
--- app/Views/Templates/main_layout.php
+++ app/Views/Templates/main_layout.php
@@ -4,6 +4,7 @@
         <title><?= $title ?? 'L’Air Pur' ?></title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="<?= base_url('assets/img/LogoPrincipal.png') ?>">
         <link rel="stylesheet" href="assets/css/Style-Home.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
